ad75646df779866ce1b3089f86a9ce2f97e888f5
Alexander Dormann <alexdo@alexdo.de>
2015-05-16 19:32:33 +0200

    Manually trigger AdminLTE resize callbacks after the user navigated within react-router

--- public/app/components/ContentComponent.js
+++ public/app/components/ContentComponent.js
@@ -2,8 +2,14 @@ var React = require('react');
 var Router = require('react-router'); // or var Router = ReactRouter; in browsers
 var RouteHandler = Router.RouteHandler;
 
+var $ = window.jQuery;
 
 var ContentComponent = React.createClass({
+    componentDidUpdate: function() {
+        $.AdminLTE.layout.fix();
+        $.AdminLTE.layout.fixSidebar();
+    },
+
     render: function() {
         return (
             <RouteHandler />
