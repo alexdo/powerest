dc72285e961b84a37bb8d182cf9e388abf182a92
Alexander Dormann <alexdo@alexdo.de>
2015-05-16 15:15:00 +0200

    Implement css autoprefixer

--- webpack.config.js
+++ webpack.config.js
@@ -6,10 +6,10 @@ module.exports = {
     module: {
         loaders: [
             { test: /\.js$/, exclude: /(node_modules|public\/components|public\/dist)/, loader: 'babel-loader' },
-            { test: /\.less$/, loader: 'style-loader!css-loader!less-loader' }, // use ! to chain loaders
-            { test: /\.(scss|sass)$/, loader: 'style-loader!css-loader!sass-loader' },
-            { test: /\.css$/, loader: 'style-loader!css-loader' },
-            { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' } // inline base64 URLs for <=8k images, direct URLs for the rest
+            { test: /\.less$/, loader: 'style-loader!css-loader!autoprefixer-loader!less-loader' }, // use ! to chain loaders
+            { test: /\.(scss|sass)$/, loader: 'style-loader!css-loader!autoprefixer-loader!sass-loader' },
+            { test: /\.css$/, loader: 'style-loader!css-loader!autoprefixer-loader' },
+            { test: /\.(png|jpg)$/, loader: 'url-loader?limit=10240' } // inline base64 URLs for <=8k images, direct URLs for the rest
         ]
     }
 };
