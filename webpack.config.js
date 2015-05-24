015693ae432d7d94516fcc313f1986ce874f1378
Alexander Dormann <alexdo@alexdo.de>
2015-05-24 11:13:34 +0200

    Define the current versions and head in webpack for use within our app

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,35 @@
+var webpack = require('webpack');
+var fs = require('fs');
+
+var gitHead = '';
+var gitRev = '';
+
+fs.readFile('./.git/HEAD', 'utf8', function (err, data) {
+    if (err) {
+        console.log(
+            'Unable to read .git/HEAD',
+            'The GIT_HEAD and GIT_REVISION constants will yield an empty string for this build.',
+            err
+        );
+    } else {
+        gitHead = data.split(': ').pop().trim();
+    }
+});
+
+if (gitHead.length > 0) {
+    fs.readFile('./.git/' + gitHead, 'utf8', function (err, data) {
+        if (err) {
+            console.log(
+                'Unable to read .git/' + gitHead,
+                'The GIT_REVISION constant will yield an empty string for this build.',
+                err
+            );
+        }
+
+        gitRev = data.trim();
+    });
+}
+
 module.exports = {
     entry: './public/app/main.js',
     //devtool: '#inline-source-map',
@@ -12,5 +44,12 @@ module.exports = {
             { test: /\.css$/, loader: 'style-loader!css-loader!autoprefixer-loader' },
             { test: /\.(png|jpg)$/, loader: 'url-loader?limit=10240' } // inline base64 URLs for <=8k images, direct URLs for the rest
         ]
-    }
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            VERSION: JSON.stringify(require('./package.json').version),
+            GIT_HEAD: gitHead,
+            GIT_REVISION: gitRev
+        })
+    ]
 };
