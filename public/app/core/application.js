2b446365347317b8fb5cccc14198b48fa43582ad
Alexander Dormann <alexdo@alexdo.de>
2015-05-24 12:04:20 +0200

    Move admin-lte js deps into npm/webpack

--- public/app/core/application.js
+++ public/app/core/application.js
@@ -1,11 +1,18 @@
-var React = require('react');
-
 var HeaderComponent = require('../components/HeaderComponent');
 var SidebarComponent = require('../components/SidebarComponent');
 var ContentComponent = require('../components/ContentComponent');
 var FooterComponent = require('../components/FooterComponent');
 
 require('./application.scss');
+
+// AdminLTE compatibility hacks; (does not support common js)
+window.jQuery = require('jquery');
+window.$ = window.jQuery;
+
+require('bootstrap');
+require('datatables');
+require('datatables-bootstrap3-plugin');
+require('../../components/admin-lte/dist/js/app');
 
 var App = React.createClass({
     render: function() {
--- webpack.config.js
+++ webpack.config.js
@@ -54,7 +54,7 @@ module.exports = {
         new webpack.ProvidePlugin({
             $: 'jquery',
             _: 'underscore',
-            react: 'react'
+            React: 'react'
         })
     ]
 };
