1788c4d89f8b0f986923d161ca7a7e3b929865c1
Alexander Dormann <alexdo@alexdo.de>
2015-05-25 12:39:43 +0200

    Add generic validator

---
+++ public/app/core/validator.js
@@ -0,0 +1,26 @@
+var Config = require('../config.js');
+var ipaddr = require('ipaddr.js');
+
+class Validator {
+    static nameserver(subject) {
+        return Validator.domain(subject) || Validator.ip(subject) || Validator.ip6(subject);
+    }
+
+    static domain(subject) {
+        return !!subject.match(/\..*?\./)
+    }
+
+    static ip(subject) {
+        return ipaddr.IPv4.isValid(subject);
+    }
+
+    static ip6(subject) {
+        return ipaddr.IPv6.isValid(subject);
+    }
+
+    static email(subject) {
+        return subject.match(/@/) && subject.match(/\./)
+    }
+}
+
+module.exports = Validator;
